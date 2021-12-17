2b76ed01c578e4bede976ac4800c52ee6cfaefb9
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2021-12-17 10:47:52 -0500

    Add `createMeter`  helper function.

---
+++ test/helpers.js
@@ -0,0 +1,21 @@
+/*!
+ * Copyright (c) 2021 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+const bedrock = require('bedrock');
+const {Ed25519Signature2020} = require('@digitalbazaar/ed25519-signature-2020');
+const {agent} = require('bedrock-https-agent');
+const {ZcapClient} = require('@digitalbazaar/ezcap');
+
+exports.createMeter = async ({meter, invocationSigner}) => {
+  const zcapClient = new ZcapClient({
+    agent,
+    invocationSigner,
+    SuiteClass: Ed25519Signature2020
+  });
+
+  const meterService = `${bedrock.config.server.baseUri}/meters`;
+
+  return zcapClient.write({url: meterService, json: meter});
+};
