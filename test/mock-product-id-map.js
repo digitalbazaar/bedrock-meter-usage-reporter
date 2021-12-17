1229c49756ad1c974cef7ed4a3fee5d6cd997b73
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2021-12-17 10:47:18 -0500

    Setup handlers.

---
+++ test/mock-product-id-map.js
@@ -0,0 +1,17 @@
+/*!
+ * Copyright (c) 2021 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+const data = {};
+module.exports = data;
+
+// mock product IDs and reverse lookup for webkms/edv/etc service products
+data.productIdMap = new Map([
+  // webkms service
+  ['webkms', 'urn:uuid:80a82316-e8c2-11eb-9570-10bf48838a41'],
+  ['urn:uuid:80a82316-e8c2-11eb-9570-10bf48838a41', 'webkms'],
+  // edv service
+  ['edv', 'urn:uuid:dbd15f08-ff67-11eb-893b-10bf48838a41'],
+  ['urn:uuid:dbd15f08-ff67-11eb-893b-10bf48838a41', 'edv']
+]);
--- test/test.js
+++ test/test.js
@@ -5,8 +5,34 @@ const bedrock = require('bedrock');
 require('bedrock-mongodb');
 require('bedrock-https-agent');
 require('bedrock-server');
-require('bedrock-app-identity');
+const {getServiceIdentities} = require('bedrock-app-identity');
+require('bedrock-kms-http');
+require('bedrock-edv-storage');
 require('bedrock-meter-usage-reporter');
+const {handlers} = require('bedrock-meter-http');
+
+const mockData = require('./mock-product-id-map');
+
+bedrock.events.on('bedrock.init', async () => {
+  /* Handlers need to be added before `bedrock.start` is called. These are
+      no-op handlers to enable meter usage without restriction */
+  handlers.setCreateHandler({
+    handler({meter} = {}) {
+      // use configured meter usage reporter as service ID for tests
+      const clientName = mockData.productIdMap.get(meter.product.id);
+      const serviceIdentites = getServiceIdentities();
+      const serviceIdentity = serviceIdentites.get(clientName);
+      if(!serviceIdentity) {
+        throw new Error(`Could not find identity "${clientName}".`);
+      }
+      meter.serviceId = serviceIdentity.id;
+      return {meter};
+    }
+  });
+  handlers.setUpdateHandler({handler: ({meter} = {}) => ({meter})});
+  handlers.setRemoveHandler({handler: ({meter} = {}) => ({meter})});
+  handlers.setUseHandler({handler: ({meter} = {}) => ({meter})});
+});
 
 require('bedrock-test');
 bedrock.start();
